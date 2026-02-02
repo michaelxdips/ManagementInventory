5541bddc9921be943badfe9dd1f383c0bfc2d584
michaelxdips <142143611+michaelxdips@users.noreply.github.com>
2026-02-02 14:19:34 +0700

    Delete backend directory
    Needed replace for new backend logic

--- backend/src/routes/barangKosong.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/barangKosong.controller');
-
-const router = express.Router();
-
-router.get('/', controller.index);
-router.post('/', controller.store);
-
-module.exports = router;
